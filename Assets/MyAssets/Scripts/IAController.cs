086c8ee47ec83f81f5829a744eb994da03cb9e7e
osukadesu <64621867+osukadesu@users.noreply.github.com>
2021-11-15 18:01:28 -0500

    IAController

--- Assets/MyAssets/Scripts/IAController.cs
+++ Assets/MyAssets/Scripts/IAController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class IAController : MonoBehaviour
 {
+    public int time;
+    public float speed = 5f;
+    float y;
+    public bool rotime;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        time += 1;
+        transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
+        transform.Rotate(new Vector3(0, y, 0));
+        if (time >= Random.Range(100, 2500))
+        {
+            Rotatar();
+            rotime = true;
+            time = 0;
+        }
+        if (rotime == true)
+        {
+            if (time >= Random.Range(50, 100))
+            {
+                y = 0;
+                rotime = false;
+            }
+        }
+    }
+    public void Rotatar()
+    {
+        y = Random.Range(-3, 3);
     }
 }
