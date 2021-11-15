84fa5f2650ea63332a8feba164f48e927ef1eeee
osukadesu <64621867+osukadesu@users.noreply.github.com>
2021-11-15 18:50:38 -0500

    NavMesh

--- Assets/MyAssets/Scripts/IAController.cs
+++ Assets/MyAssets/Scripts/IAController.cs
@@ -1,40 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class IAController : MonoBehaviour
 {
     public int time;
     public float speed = 5f;
     float y;
     public bool rotime;
+    public bool Attack;
+    public bool idle;
+    public NavMeshAgent agent;
+    public Animator anim;
 
+    public int state = 1;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         time += 1;
-        transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
-        transform.Rotate(new Vector3(0, y, 0));
-        if (time >= Random.Range(200, 2500))
+        if (state == 1)
         {
-            Rotatar();
-            rotime = true;
-            time = 0;
+            Attack = true;
+            idle = false;
         }
-        if (rotime == true)
+        else if (state == 2)
         {
-            if (time >= Random.Range(15, 40))
+            Attack = false;
+            idle = true;
+        }
+        if (idle == true)
+        {
+            transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
+            transform.Rotate(new Vector3(0, y, 0));
+            anim.SetBool("Idle",true);
+            if (time >= Random.Range(200, 2500))
             {
-                y = 0;
-                rotime = false;
+                Rotatar();
+                rotime = true;
+                time = 0;
+            }
+            if (rotime == true)
+            {
+                if (time >= Random.Range(15, 40))
+                {
+                    y = 0;
+                    rotime = false;
+                }
+            }
+        }
+        else if (Attack == true)
+        {
+            agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+            if (Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position) > 30)
+            {
+                state = 1;
             }
         }
     }
